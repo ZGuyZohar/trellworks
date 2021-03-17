02fc877a6e0810507af26651a39699cec6141346
Almog <almoglemberg@gmail.com>
2021-03-17 17:36:54 +0200

    load board line fixed

--- frontend/src/views/board.vue
+++ frontend/src/views/board.vue
@@ -1,46 +1,42 @@
 <template>
-    <section v-if="currBoard" class="board">
-    <board-header/>
-    <h1>Board - {{currBoard.title}}</h1>
+  <section v-if="currBoard" class="board">
+    <board-header />
+    <h1>Board - {{ currBoard.title }}</h1>
     <group v-for="group in groups" :key="group.id" :group="group" />
-
-    </section>
+  </section>
 </template>
 
 <script>
-import boardHeader from '@/cmps/board/board-header'
-import group from '@/cmps/board/group'
+import boardHeader from "@/cmps/board/board-header";
+import group from "@/cmps/board/group";
 
-export default { 
-    data(){
-        return {
-            groups: [],
-        }
+export default {
+  data() {
+    return {
+      groups: [],
+    };
+  },
+  computed: {
+    boardId() {
+      return this.$route.params.boardId;
     },
-    computed: {
-        boardId(){
-            return this.$route.params.boardId
-        },
-        currBoard(){
-            return this.$store.getters.currBoard
-        }
-    },    
-    methods: {
-        async loadBoard(){
-            await this.$store.dispatch({type: 'getBoard', boardId: this.boardId})
-            this.currBoard.groups.forEach(group => this.groups.push(group))               
-        },
-        async getGroup(group){
-
-        }
+    currBoard() {
+      return this.$store.getters.currBoard;
     },
-    async created() { 
-        // await this.loadBoard()
-        this.$store.dispatch({type: 'loadBoards'})
+  },
+  methods: {
+    async loadBoard() {
+      await this.$store.dispatch({ type: "getBoard", boardId: this.boardId });
+      this.currBoard.groups.forEach((group) => this.groups.push(group));
     },
-    components: {
-        boardHeader,
-        group
-    }
-}
+    async getGroup(group) {},
+  },
+  async created() {
+    await this.loadBoard();
+  },
+  components: {
+    boardHeader,
+    group,
+  },
+};
 </script>
