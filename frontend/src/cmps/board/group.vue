68ec4d54753ee2b00311a8cf20a613294f40de94
Almog <almoglemberg@gmail.com>
2021-03-17 20:12:49 +0200

    push

--- frontend/src/cmps/board/group.vue
+++ frontend/src/cmps/board/group.vue
@@ -1,28 +1,37 @@
 <template>
   <section v-if="group" class="group">
     <section class="flex group-header">
-      <p class="group-title">{{ group.title }}</p><span>X</span>
+      <p class="group-title">{{ group.title }}</p>
+      <span>X</span>
     </section>
     <task-preview v-for="task in group.task" :key="task.id" :task="task" />
-    <span @click="addTask">+add task</span>
+    <span v-if="!isAddingTask" @click="addTask">+add task</span>
+    <template v-if="isAddingTask" class="add-task">
+      <textarea placeholder="Your task title here..." rows="3"></textarea>
+    </template>
   </section>
 </template>
 
 <script>
-  import taskPreview from "@/cmps/task/task-preview";
-  export default {
-    props: {
-      group: {
-        type: Object,
-      },
+import taskPreview from "@/cmps/task/task-preview";
+export default {
+  props: {
+    group: {
+      type: Object,
     },
-    methods: {
-      addTask() {
-        console.log("adding task");
-      },
+  },
+  data() {
+    return {
+      isAddingTask: false,
+    };
+  },
+  methods: {
+    addTask() {
+      this.isAddingTask = true;
     },
-    components: {
-      taskPreview,
-    },
-  };
+  },
+  components: {
+    taskPreview,
+  },
+};
 </script>
--- frontend/src/views/task-details.vue
+++ frontend/src/views/task-details.vue
@@ -1,29 +1,27 @@
 <template>
-    <section @click.self="closeModal" class="details-modal">
-        <div class="details">
-                {{currTask}}
-        </div>
-    </section>
+  <section @click.self="closeModal" class="dark-screen">
+    <div class="task-details">
+      {{ currTask }}
+    </div>
+  </section>
 </template>
 
 <script>
-
 export default {
-    computed: {
-        currTask(){
-            return this.$store.getters.currTask
-        },
+  computed: {
+    currTask() {
+      return this.$store.getters.currTask;
     },
-    methods: {
-        closeModal(){
-            this.$router.push(`/board/${this.$route.params.boardId}`)
-        }
+  },
+  methods: {
+    closeModal() {
+      this.$router.push(`/board/${this.$route.params.boardId}`);
     },
-    created(){
-        const taskId = this.$route.params.taskId
-        this.$store.commit({ type: 'getTask', taskId })
-        console.log(this.currTask);
-    }
-
-}
+  },
+  created() {
+    const taskId = this.$route.params.taskId;
+    this.$store.commit({ type: "getTask", taskId });
+    console.log(this.currTask);
+  },
+};
 </script>
