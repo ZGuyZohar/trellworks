9cf75321ce4be59e8922ba7b0e2b6be2aebb5356
Almog <almoglemberg@gmail.com>
2021-03-17 18:46:29 +0200

    add task span added

--- frontend/src/cmps/board/group.vue
+++ frontend/src/cmps/board/group.vue
@@ -2,6 +2,7 @@
   <section v-if="group" class="group">
     <p class="group-title">{{ group.title }}</p>
     <task-preview v-for="task in group.task" :key="task.id" :task="task" />
+    <span @click="addTask">+add task</span>
   </section>
 </template>
 
