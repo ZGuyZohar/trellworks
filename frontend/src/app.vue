cb5a3811d092270371a16b201fe214f73e6d0e79
Guy Zohar Abecassis <zguyzohar@gmail.com>
2021-03-17 12:08:52 +0200

    first commit

---
+++ frontend/src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <app-header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import appHeader from './cmps/app-header'
+
+export default {
+  components: {
+    appHeader
+  }
+}
+</script>
+
