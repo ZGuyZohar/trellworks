a6123a79af1d988cb739ca5b05d7331ba4b13cf3
Gali Nickel <gali.nickel95@gmail.com>
2021-03-17 20:18:39 +0200

    starting work on group

--- frontend/src/cmps/board/group.vue
+++ frontend/src/cmps/board/group.vue
@@ -1,7 +1,7 @@
 <template>
   <section v-if="group" class="group">
     <section class="flex group-header">
-      <p class="group-title">{{ group.title }}</p><span>X</span>
+      <p class="group-title">{{ group.title }}</p><span @click="removeGroup(group.id)">X</span>
     </section>
     <task-preview v-for="task in group.task" :key="task.id" :task="task" />
     <span @click="addTask">+add task</span>
@@ -20,6 +20,10 @@
       addTask() {
         console.log("adding task");
       },
+      removeGroup(groupId) {
+        console.log('removing group: ', groupId);
+        this.$store.dispatch({type: 'removeGroup',groupId: groupId})
+      }
     },
     components: {
       taskPreview,
