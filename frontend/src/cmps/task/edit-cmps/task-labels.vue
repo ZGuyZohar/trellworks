f399b74fb7ba2a9f0ddb50ef1e323bd4d4be54ae
zguyzohar <zguyzohar@gmail.com>
2021-03-18 15:00:02 +0200

    add label

---
+++ frontend/src/cmps/task/edit-cmps/task-labels.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="task-labels">
+      <input type="text" class="filter-labels" placeholder="Search labels...">
+      <h3>Labels</h3>
+      
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
