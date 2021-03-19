fde5cef0db308888b75f58be5fa4dfa197952821
Almog <almoglemberg@gmail.com>
2021-03-19 23:26:11 +0200

    push

--- frontend/src/cmps/task/edit-cmps/member.vue
+++ frontend/src/cmps/task/edit-cmps/member.vue
@@ -4,21 +4,4 @@
 
 
 <script>
-import { utilService } from "@/services/util.service.js";
-export default {
-  props: {
-    task: Object,
-  },
-  data() {
-    return {
-      taskToEdit: utilService.deepCopy(this.task),
-    };
-  },
-  computed: {
-    currBoard() {
-      return utilService.deepCopy(this.$store.getters.currBoard);
-    },
-  },
-  methods: {},
-};
 </script>
