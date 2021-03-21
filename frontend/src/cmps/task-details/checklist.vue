2bf3e0eb52a07f22acd983cdd78d90ae5eb5525f
Almog <almoglemberg@gmail.com>
2021-03-21 14:43:43 +0200

    further checklist

--- frontend/src/cmps/task-details/checklist.vue
+++ frontend/src/cmps/task-details/checklist.vue
@@ -1,18 +1,54 @@
 <template>
   <section class="task-checklist">
     <p><i class="far fa-check-square"></i> {{ checklist.title }}</p>
-    <button class="btn-gray">Add item</button>
+
+    <div v-for="todo in checklist.todos" :key="todo.id">
+      <todo-item :todo="todo" />
+    </div>
+
+    <button v-if="!isAddingItem" class="btn-gray" @click="isAddingItem = true">
+      Add item
+    </button>
+    <form v-if="isAddingItem" @submit.prevent="addTodo">
+      <input placeholder="Add an item" v-model="todoToAdd.title" />
+      <button type="submit" class="btn-success">Add</button>
+    </form>
   </section>
 </template>
 
 
 <script>
+import { utilService } from "../../services/util.service";
+import todoItem from "../task-details/todo-item";
+
 export default {
   props: {
     checklist: Object,
+    task: Object,
   },
-  created() {
-    console.log(this.checklist);
+  data() {
+    return {
+      isAddingItem: false,
+      taskToEdit: JSON.parse(JSON.stringify(this.task)),
+      checklistToEdit: JSON.parse(JSON.stringify(this.checklist)),
+      todoToAdd: { title: "", isDone: false },
+    };
   },
+  methods: {
+    addTodo() {
+      this.isAddingItem = false;
+      this.todoToAdd.id = utilService.makeId();
+      this.checklistToEdit.todos.push(this.todoToAdd);
+      const idx = this.taskToEdit.checklists.findIndex(
+        (cl) => cl.id === this.checklist.id
+      );
+      if (idx !== -1) {
+        this.taskToEdit.checklists.splice(idx, 1, this.checklistToEdit);
+        this.$emit("updateTask", this.taskToEdit);
+        this.todoToAdd = { title: "", isDone: false };
+      }
+    },
+  },
+  components: { todoItem },
 };
 </script>
