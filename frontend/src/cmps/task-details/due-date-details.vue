3067fbb4c7b42d18640977b70256dc29669bd3ab
Gali Nickel <gali.nickel95@gmail.com>
2021-03-22 14:10:04 +0200

    timedue CSS

--- frontend/src/cmps/task-details/due-date-details.vue
+++ frontend/src/cmps/task-details/due-date-details.vue
@@ -46,9 +46,9 @@ export default ({
 		deteremineDate() {
 			const dateStr = (moment(this.task.dueDate).fromNow());
 			if (dateStr.includes('ago')) return this.dueClass = "overdue"
-			else if (dateStr.includes('days') || dateStr.includes('day')) this.dueClass = "days"
+			else if (dateStr.includes('days')) this.dueClass = "days"
 			else if (dateStr.includes('weeks') || dateStr.includes('week')) this.dueClass = "weeks"
-			else if (dateStr.includes('hours') || dateStr.includes('hour')) this.dueClass = "hours"
+			else if (dateStr.includes('hours') || dateStr.includes('hour') || dateStr.includes('day')) this.dueClass = "hours"
 			else this.dueClass = "months"
 		},
 		toggleComlpetion() {
